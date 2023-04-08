9aad26257e14b62857ab6b2b2d35e8b2cb2a1369
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2023-04-08 20:18:26 +0200

    Buffer implemented.

---
+++ PxCs.Tests/Phoenix/Csharp/Interface/BufferTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+
+namespace Phoenix.Csharp.Interface
+{
+    public class BufferTest : PhoenixTest
+    {
+        [Fact]
+        public void Test_open_file()
+        {
+            var bufferPtr = Buffer.pxBufferMmap(GetAssetPath("Data/textures_bikini.VDF"));
+
+            Assert.True(bufferPtr != IntPtr.Zero, "Buffer has no pointer and therefore no data. Does your file exist?");
+
+            Buffer.pxBufferDestroy(bufferPtr);
+        }
+    }
+}
---
+++ PxCs/Phoenix/Csharp/Interface/Buffer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phoenix.Csharp.Interface
+{
+    public class Buffer
+    {
+        private const string DLLNAME = Phoenix.DLLNAME;
+
+        [DllImport(DLLNAME)] public static extern IntPtr pxBufferCreate(out IntPtr bytes, ulong size);
+        [DllImport(DLLNAME)] public static extern IntPtr pxBufferMmap(string filePath);
+        [DllImport(DLLNAME)] public static extern void pxBufferDestroy(IntPtr buffer);
+    }
+}
