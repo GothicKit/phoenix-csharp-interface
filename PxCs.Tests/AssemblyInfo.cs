93acdc7da2fade3f6028815390173f35ef7bd2c4
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2023-04-11 19:29:15 +0200

    Tests now run serial which is more more to the actual game behavior: Parse once, destroy multiple times.

---
+++ PxCs.Tests/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using Xunit;
+
+// Disable parallelization as phoenix values might be dependent on another.
+// (And a gothic game will also not run twice in parallel tbh.)
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
