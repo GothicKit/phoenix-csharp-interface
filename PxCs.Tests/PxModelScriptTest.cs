1216dd0c330333ee3fa7e4b41208b39b3e1a186b
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2023-04-21 11:33:55 +0200

    Added simple modelscript test.

---
+++ PxCs.Tests/PxModelScriptTest.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace PxCs.Tests
+{
+    public class PxModelScriptTest : PxPhoenixTest
+    {
+        
+        [Fact]
+        public void Test_load_model()
+        {
+            var vdfPtr = LoadVdf("Data/anims.VDF");
+
+            var mdsPtr = PxModelScript.pxMdsLoadFromVdf(vdfPtr, "HUMANS.MDS");
+
+            Assert.True(mdsPtr != System.IntPtr.Zero);
+
+            PxModelScript.pxMdsDestroy(mdsPtr);
+            DestroyVdf(vdfPtr);
+        }
+    }
+}
---
+++ PxCs/PxModelScript.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PxCs
+{
+    public static class PxModelScript
+    {
+        private const string DLLNAME = PxPhoenix.DLLNAME;
+
+        [DllImport(DLLNAME)] public static extern IntPtr pxMdsLoad(IntPtr buffer);
+        [DllImport(DLLNAME)] public static extern IntPtr pxMdsLoadFromVdf(IntPtr vdf, string name);
+        [DllImport(DLLNAME)] public static extern void pxMdsDestroy(IntPtr mdm);
+    }
+}
