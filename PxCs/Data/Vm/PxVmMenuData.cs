cb95a543351eb974894e131220a76a73ad892859
Jucan Andrei Daniel <setrofex@gmail.com>
2023-08-10 10:00:53 +0300

    Add c_menu and c_menu_item data

---
+++ PxCs/Data/Vm/PxVmMenuData.cs
@@ -0,0 +1,19 @@
+namespace PxCs.Data.Vm
+{
+    public class PxVmMenuData : PxVmData
+    {
+        public string? backPic;
+        public string? backWorld;
+        public int posX;
+        public int posY;
+        public int dimX;
+        public int dimY;
+        public int alpha;
+        public string? musicTheme;
+        public int eventTimerMsec;
+        public string[]? items;
+        public uint flags;
+        public int defaultOutgame;
+        public int defaultIngame;
+    }
+}
---
+++ PxCs/Data/Vm/PxVmMenuItemData.cs
@@ -0,0 +1,33 @@
+namespace PxCs.Data.Vm
+{
+    public class PxVmMenuItemData : PxVmData
+    {
+
+        public string? fontname;
+        public string[]? text;
+        public string? backpic;
+        public string? alphamode;
+        public int alpha;
+        public uint type;
+        public int[]? onSelAction;
+        public string[]? onSelActionS;
+        public string? onChgSetOption;
+        public string? onChgSetOptionSection;
+        public int[]? onEventAction;
+        public int posX;
+        public int posY;
+        public int dimX;
+        public int dimY;
+        public float sizeStartScale;
+        public uint flags;
+        public float openDelayTime;
+        public float openDuration;
+        public float[]? userFloat;
+        public string[]? userString;
+        public int frameSizex;
+        public int frameSizey;
+        public string? hideIfOptionSectionSet;
+        public string? hideIfOptionSet;
+        public int hideOnValue;
+    }
+}
